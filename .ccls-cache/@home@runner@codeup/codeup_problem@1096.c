40edc24317e54b933be2dfd48f0310e1a949ccf0
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-06-13 23:58:52 +0000

    2024/6/14

---
+++ .ccls-cache/@home@runner@codeup/codeup_problem@1096.c
@@ -0,0 +1,18 @@
+// #include <stdio.h>
+
+// int main() {
+//   int n, x, y, arr[20][20]={0,}; //n, x, y 선언 및 배열 초기화
+//   scanf("%d", &n); //n 입력받기
+
+//   for (int i=0; i<n; i++) { //좌표를 입력받아 좌표의 위치를 1로 변경
+//       scanf("%d %d", &x, &y);
+//       arr[x][y] = 1;
+//   }
+
+//   for (int i=1; i<=19; i++) { //이차원 배열 출력
+//       for (int j=1; j<=19; j++)
+//           printf("%d ", arr[i][j]);
+//       printf("\n");
+//   }
+//   return 0;
+// }
---
+++ .ccls-cache/@home@runner@codeup/codeup_problem@1132.c
@@ -0,0 +1,7 @@
+// #include<stdio.h>
+// int main(void) {
+//   char what[20]; //문자 선언
+//   scanf("%s", &what); //문자 입력
+//   printf("%s", what); //문자 출력
+//   return 0;
+// }
