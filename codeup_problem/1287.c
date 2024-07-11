adb4fc8a01d9a5a90efcb657218f7a336c9d08a5
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-07-11 11:37:51 +0000

    c

--- codeup_problem/1287.c
+++ codeup_problem/1287.c
@@ -3,10 +3,13 @@
 //   int number; //입력받을 숫자 선언
 //   scanf("%d", &number); //숫자 입력받기
 //   for(int i = 1; i <= 9; i++) { //구구단 9번까지 반복
-//     for(int j = 1; j <= i * number; j++) { //입력받은 숫자 X 구구단번호(i) 만큼의 *을 반복출력
+//     for(int j = 1; j <= i * number; j++) { //입력받은 숫자 X 구구단번호(i)
+//     만큼의 *을 반복출력
 //       printf("*");
 //     }
 //     printf("\n"); //줄바꿈
 //   }
 //   return 0;
 // }
+//
+//
