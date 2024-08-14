32a70ffe562f16fb1693bb78170283dc00e36e7d
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-08-14 13:42:31 +0000

    2024/8/14 - 22 : 42

---
+++ codeup_problem/2025.c
@@ -0,0 +1,11 @@
+// #include <stdio.h>
+// int main(void) {
+//     int a,b,c,d,e,i;
+//     scanf("%1d%1d%1d%1d%1d", &a, &b, &c, &d, &e);
+//     printf("[%d]\n",10000 * a);
+//     printf("[%d]\n",1000 * b);
+//     printf("[%d]\n",100 * c);
+//     printf("[%d]\n",10 * d);
+//     printf("[%d]\n",e);
+//     return 0;
+// }
