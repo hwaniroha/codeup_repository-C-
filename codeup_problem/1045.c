0399bfee0605698f009aca1de46838593013823a
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-09-08 10:59:05 +0000

    2024/9/8 - 19 : 59

---
+++ codeup_problem/1045.c
@@ -0,0 +1,12 @@
+// #include <stdio.h>
+// int main(void) {
+//     int n1,n2 = 0;
+//     scanf("%d %d",&n1,&n2);
+//     printf("%d\n",n1 + n2);
+//     printf("%d\n",n1 - n2);
+//     printf("%d\n",n1 * n2);
+//     printf("%d\n",n1 / n2);
+//     printf("%d\n",n1 % n2);
+//     printf("%.2f\n",((float)n1 / (float)n2));
+//     return 0;
+// }
