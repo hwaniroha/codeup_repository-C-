61084a582b67fcd5bb22533f486bd5bca4123ed4
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-09-09 11:11:05 +0000

    2024/9/9 - 20 : 11

---
+++ .ccls-cache/@home@runner@codeup/codeup_problem@1046.c
@@ -0,0 +1,9 @@
+// #include <stdio.h>
+// int main(void) {
+//     int n1,n2,n3 = 0;
+//     scanf("%d %d %d",&n1,&n2,&n3);
+//     int sum = n1 + n2 + n3;
+//     printf("%d\n", sum);
+//     printf("%.1f\n", (float)sum / 3);
+//     return 0;
+// }
