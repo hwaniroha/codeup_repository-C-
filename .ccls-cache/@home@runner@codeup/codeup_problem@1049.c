80896cb9b4cd6da0ff24e4d98652de7daa153291
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-09-14 12:19:43 +0000

    2024/9/14 - 21 : 19

---
+++ .ccls-cache/@home@runner@codeup/codeup_problem@1049.c
@@ -0,0 +1,11 @@
+// #include <stdio.h>
+// int main(void) {
+//     int num1,num2 = 0;
+//     scanf("%d  %d", &num1,&num2);
+//     if (num1 <= num2) {
+//         printf("%d",0);
+//     }
+//     else printf("%d",1);
+
+//     return 0;
+// }
