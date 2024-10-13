7245b9d490276d25d4e53b405ee0d8192d00d044
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-10-13 12:35:10 +0000

    2024/10/13 - 21 : 35

---
+++ codeup_problem/1065.c
@@ -0,0 +1,9 @@
+// #include <stdio.h>
+// int main(void) {
+//     int a,b,c;
+//     scanf("%d %d %d", &a, &b, &c);
+//     (a%2)?0:(printf("%d\n",a));
+//     (b%2)?0:(printf("%d\n",b));
+//     (c%2)?0:(printf("%d\n",c));
+//     return 0;
+// }
