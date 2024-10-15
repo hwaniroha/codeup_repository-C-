b077f133744a2d8e0b6da6246e9d8d75c8ad1064
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-10-15 14:54:39 +0000

    2024/10/15 - 23 : 54

---
+++ .ccls-cache/@home@runner@codeup/codeup_problem@1066.c
@@ -0,0 +1,10 @@
+// #include <stdio.h>
+// int main(void) {
+//     int a,b,c;
+//     scanf("%d %d %d", &a, &b, &c);
+//     printf("%s\n", a%2?"odd":"even");
+//     printf("%s\n", b%2?"odd":"even");
+//     printf("%s\n", c%2?"odd":"even");
+//     return 0;
+
+// }
