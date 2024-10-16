e65642445d6d4cf7083c80bdd0142cac3694439a
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-10-16 01:15:29 +0000

    2024/10/16 - 10 : 15

---
+++ .ccls-cache/@home@runner@codeup/codeup_problem@1067.c
@@ -0,0 +1,13 @@
+// #include <stdio.h>
+// int main(void) {
+//     int a;
+//     scanf("%d", &a);
+//     printf("%s\n", a>0?"plus":"minus");
+//     printf("%s\n", a%2?"odd":"even");
+
+
+
+
+//     return 0;
+
+// }
