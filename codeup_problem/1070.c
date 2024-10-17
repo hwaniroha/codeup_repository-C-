ee327d9251eef6b8cb95a80a14378585e63fbe6d
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-10-17 06:18:20 +0000

    2024/10/17 - 15 : 18

---
+++ codeup_problem/1070.c
@@ -0,0 +1,10 @@
+// #include <stdio.h>
+// int main(void) {
+//     int num;
+//     scanf("%d",&num);
+//     if (num < 3 || num == 12) printf("winter");
+//     else if (num >= 3 && num <= 5) printf("spring");
+//     else if (num >= 6 && num <= 8) printf("summer");
+//     else printf("fall");
+//     return 0;
+// }
