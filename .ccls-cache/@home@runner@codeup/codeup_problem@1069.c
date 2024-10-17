11b47e5f69b8230bc3d0e9f37ea307bf708c6778
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-10-17 05:58:14 +0000

    2024/10/17 - 14 : 58

---
+++ .ccls-cache/@home@runner@codeup/codeup_problem@1069.c
@@ -0,0 +1,11 @@
+// #include <stdio.h>
+// int main(void) {
+//     char ch;
+//     scanf("%c",&ch);
+//     if (ch == 'A') printf("best!!!");
+//     else if (ch == 'B') printf("good!!");
+//     else if (ch == 'C') printf("run!");
+//     else if (ch == 'D') printf("slowly~");
+//     else printf("what?");
+//     return 0;
+// }
