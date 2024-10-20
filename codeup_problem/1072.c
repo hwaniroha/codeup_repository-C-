379c7a263d2ac8bf88de07520e53abc3db433782
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-10-20 13:55:16 +0000

    2024/10/20 - 22 : 55

---
+++ codeup_problem/1072.c
@@ -0,0 +1,16 @@
+// #include <stdio.h>
+// int main(void) {
+//     int num1;
+//     int num2;
+//     int i = 0;
+//     scanf("%d",&num1);
+//     for (;;) {
+//         scanf("%d",&num2);
+//         if (i == num1) {
+//             break;
+//         }
+//         printf("%d\n",num2);
+//         i++;
+//     }
+//     return 0;
+// }
