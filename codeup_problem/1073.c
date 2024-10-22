9a6278d01baf18273804b98af2d8aa439aafa2de
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-10-22 08:42:07 +0000

    2024/10/22 - 17 : 42

---
+++ codeup_problem/1073.c
@@ -0,0 +1,14 @@
+// #include <stdio.h>
+// int main(void) {
+//     while (1) {
+//         int num;
+//         scanf("%d",&num);
+//         if (num == 0) {
+//             break;
+//         }
+//         printf("%d\n",num);
+//     }
+
+
+//     return 0;
+// }
