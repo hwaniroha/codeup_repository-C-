1e2f80f9afdacdcab2e08a2b0f06ac300acedecc
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-10-23 02:20:59 +0000

    2024/10/23 - 11 : 20

---
+++ codeup_problem/1074.c
@@ -0,0 +1,12 @@
+// #include <stdio.h>
+// int main(void) {
+//     int num1;
+//     scanf("%d",&num1);
+//     for (;;num1--) {
+//         if (num1 == 0) {
+//             break;
+//         }
+//         printf("%d\n",num1);
+//     }
+//     return 0;
+// }
