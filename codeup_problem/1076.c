15c720104b4c99916818d92c863829486b52783d
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-10-24 02:20:16 +0000

    2024/10/24 - 11 : 20

---
+++ codeup_problem/1076.c
@@ -0,0 +1,15 @@
+// #include <stdio.h>
+// int main(void) {
+//     char x, t='a';
+//     scanf("%c", &x);
+//     do
+//     {
+//       printf("%c ", t);
+//       t+=1; 
+//         if (t > x) {
+//             break;
+//         }
+//     }while(t<'z'+1); 
+
+//     return 0;
+// }
