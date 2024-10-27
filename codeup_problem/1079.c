e19a1805fec29fc6278f23960534d9e01db5b310
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-10-27 14:40:38 +0000

    2024/10/27 - 23 : 40

---
+++ codeup_problem/1079.c
@@ -0,0 +1,12 @@
+// #include <stdio.h>
+
+// int main (void)
+// {
+//     char c;
+//     do{
+//          scanf(" %c",&c);
+//          printf("%c\n",c);
+//         if(c=='q') break;
+//     }while (1);
+//     return 0;
+// }
