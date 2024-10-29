029a6dd5723e8597425e85b7cd00ad0a5ef6910a
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-10-29 14:32:04 +0000

    2024/10/29 - 23 : 32

---
+++ codeup_problem/1080.c
@@ -0,0 +1,13 @@
+// #include <stdio.h>
+
+// int main (void)
+// {
+//     int num,i,result = 0;
+//     scanf("%d",&num);
+//     while(i < num) {
+//         result += 1;
+//         i += result;
+//     }
+//     printf("%d",result);
+//     return 0;
+// }
