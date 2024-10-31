443a113b3a00d4851d9c6fe7e2e1eac285e3412e
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-10-31 23:07:42 +0000

    2024/11/1 - 8 : 07

--- codeup_problem/1085.c
+++ codeup_problem/1085.c
@@ -1,19 +1,8 @@
 // #include <stdio.h>
-// int main (void)
-// {
-//     int i, j, k, c=0;
-//     int r, g, b;
-//     scanf("%d%d%d", &r, &g, &b);
-
-//     for(i=0; i<r; i++)
-//       for(j=0; j<g; j++)
-//         for(k=0; k<b; k++)
-//         {
-//           printf("%d %d %d\n", i, j, k);
-//           c++;
-//         }
-
-//     printf("%d ", c);
-
+// int main(void) {
+//     float h,b,c,s;
+//     scanf("%f %f %f %f", &h, &b, &c, &s);
+//     float result = (h * b * c * s) / (8*1024*1024);
+//     printf("%.1f MB", (result));
 //     return 0;
 // }
