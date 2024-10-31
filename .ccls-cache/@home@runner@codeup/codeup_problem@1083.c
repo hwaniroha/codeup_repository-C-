d70a7ec02f44bc4297de2adcd6ddcf2b65d22613
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-10-31 11:25:01 +0000

    2024/10/31 - 20 : 24

---
+++ .ccls-cache/@home@runner@codeup/codeup_problem@1083.c
@@ -0,0 +1,12 @@
+// #include <stdio.h>
+// int main (void)
+// {
+//     int j;
+//     scanf("%d",&j);
+//     for(int i=1; i<=j; i++)
+//     {
+//       if(i%3==0) printf("X ");
+//       else printf("%d ",i);
+//     }
+//     return 0;
+// }
