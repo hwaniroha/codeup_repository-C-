2229544f753da0aebd01645cbafc0da83bda82c8
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-10-31 04:26:30 +0000

    2024/10/31 - 13 : 26

---
+++ codeup_problem/1082.c
@@ -0,0 +1,10 @@
+// #include <stdio.h>
+// int main (void)
+// {
+//     int c;
+//     scanf("%X", &c);
+//     for (int i = 1; i <= 15; i++) {
+//         printf("%X*%X=%X\n",c,i,c * i);
+//     }
+//     return 0;
+// }
