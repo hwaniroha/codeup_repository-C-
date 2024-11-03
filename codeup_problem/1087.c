7bdf94155d5a568164cffb4b48370cfbe1a8b718
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-11-03 13:28:01 +0000

    2024/11/3 - 22 : 27

---
+++ codeup_problem/1087.c
@@ -0,0 +1,19 @@
+// #include<stdio.h>
+
+// int main(void){
+
+//     int i,sum = 0;
+//     scanf("%d",&i);
+
+//     for(int j = 1; j <= i; j++){
+//         sum += j;
+//         if(sum >= i){
+//             printf("%d",sum);
+//             break;
+//         }
+
+//     }
+
+
+//     return 0;
+// }
