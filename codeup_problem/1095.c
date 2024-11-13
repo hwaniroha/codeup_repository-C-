d430cd11c59a6a78660bef474cef98ffcd89f933
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-11-13 06:32:24 +0000

    2024/11/13 - 15 : 32

---
+++ codeup_problem/1095.c
@@ -0,0 +1,17 @@
+// #include <stdio.h>
+
+// int main() {
+//   int n, k, i, arr[10000], min=24;
+//   scanf("%d", &n);
+//   for (i=0; i<n; i++) {
+//       scanf("%d", &k);
+//       arr[i] = k;
+//   }
+
+//   for (i=0; i<n; i++) {
+//       if (arr[i] < min)
+//           min = arr[i];
+//   }
+//   printf("%d", min);
+//   return 0;
+// }
