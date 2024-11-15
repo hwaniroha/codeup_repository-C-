adc5c28c00bca0dcb52c284a4ed2fa86e1e413ab
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-11-15 04:25:49 +0000

    2024/11/15 - 13 : 25

--- codeup_problem/1040.c
+++ codeup_problem/1040.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
-int main(void) {
-    long long int n1;
-    scanf("%lld",&n1);
-    printf("%lld",-n1);
-    return 0;
-}
+// #include <stdio.h>
+// int main(void) {
+//     long long int n1;
+//     scanf("%lld",&n1);
+//     printf("%lld",-n1);
+//     return 0;
+// }
