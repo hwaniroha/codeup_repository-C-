e923997c3ad5b5915a5f80bddf165de223d4a009
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-11-16 04:41:43 +0000

    2024/11/16 - 13 : 41

---
+++ .ccls-cache/@home@runner@codeup/codeup_problem@1098.c
@@ -0,0 +1,29 @@
+// #include <stdio.h>
+
+// int main() {
+//   int arr[101][101]={0,}, h, w, n, l, d, x, y;
+
+//   scanf("%d %d", &h, &w);
+//   scanf("%d", &n);
+//   for (int i=0; i<n; i++) {
+//       scanf("%d %d %d %d", &l, &d, &x, &y);
+
+//       if (d == 0) { //방향이 가로면
+//           for (int j=y; j<y+l ;j++) {
+//               arr[x][j] = 1;
+//           }
+//       }
+//       else { //방향이 세로면
+//           for (int j=x; j<x+l ;j++) {
+//               arr[j][y] = 1;
+//           }
+//       }
+//   }
+
+//   for (int i=1; i<=h; i++) {
+//       for (int j=1; j<=w; j++)
+//           printf("%d ", arr[i][j]);
+//       printf("\n");
+//   }
+//   return 0;
+// }
