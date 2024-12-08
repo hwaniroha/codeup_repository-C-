11bfa8715cf14f9bebca4a88b4db2d679895f293
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-12-08 12:36:45 +0000

    2024/12/8 - 21 : 36

---
+++ codeup_problem/1139.c
@@ -0,0 +1,12 @@
+// #include <stdio.h>
+// int main(void) {
+//     int a,b;
+//     scanf("%d %d",&a,&b);
+//     if ( a && b == 1) {
+//         printf("1");
+//     }
+//     else {
+//         printf("0");
+//     }
+//     return 0;
+// }
