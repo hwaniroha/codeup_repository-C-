e10003df94fb1ecac99ed5d2ad2ce8332d21d680
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-12-16 08:20:41 +0000

    2024/12/16 - 17 : 20

---
+++ .ccls-cache/@home@runner@codeup/codeup_problem@1156.c
@@ -0,0 +1,13 @@
+// #include <stdio.h>
+// int main(void) {
+//     int num = 0;
+//     scanf("%d",&num);
+
+//     if (num % 2 == 0) {
+//         printf("even");
+//     }
+//     else {
+//         printf("odd");
+//     }
+//     return 0;
+// }
