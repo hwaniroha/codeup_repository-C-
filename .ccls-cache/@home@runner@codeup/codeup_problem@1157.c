0c4131cc9e41cf17399e638604ccd6a198c66dde
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-12-17 06:05:00 +0000

    2024/12/17 - 15 : 04

---
+++ .ccls-cache/@home@runner@codeup/codeup_problem@1157.c
@@ -0,0 +1,13 @@
+// #include <stdio.h>
+// int main(void) {
+//     float num1 = 0;
+//     scanf("%f",&num1);
+
+//     if (50 < num1 && 60 > num1) {
+//         printf("win");
+//     }
+//     else {
+//         printf("lose");
+//     }
+//     return 0;
+// }
