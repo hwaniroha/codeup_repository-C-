e83291c4677626d6b953de54b0d58226e2659896
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-12-18 07:26:23 +0000

    2024/12/18 - 16 : 26

---
+++ codeup_problem/1158.c
@@ -0,0 +1,16 @@
+// #include <stdio.h>
+
+// int main(void) {
+//     int num = 0;
+//     scanf("%d",&num);
+//     if (30 <= num && num <= 40) {
+//         printf("win");
+//     }
+//     else if (60 <= num && num <= 70) {
+//         printf("win");
+//     }
+//     else {
+//         printf("lose");
+//     }
+//     return 0;
+// }
