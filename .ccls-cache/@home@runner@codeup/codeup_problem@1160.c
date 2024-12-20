4339027e649118675f1b8ece27812b935cbddbb4
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-12-20 14:44:32 +0000

    2024/12/20 - 23 : 44

---
+++ .ccls-cache/@home@runner@codeup/codeup_problem@1160.c
@@ -0,0 +1,13 @@
+// #include <stdio.h>
+
+// int main(void) {
+//     int num = 0;
+//     scanf("%d",&num);
+//     if (num == 1 || num == 3 || num == 5 || num == 7) {
+//         printf("oh my god");
+//     }
+//     else {
+//         printf("enjoy");
+//     }
+//     return 0;
+// }
