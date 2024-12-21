9588a3e7b4949df29695d7aec6752b4f3458f43a
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-12-21 12:02:39 +0000

    2024/12/21 - 21 : 02

---
+++ codeup_problem/1161.c
@@ -0,0 +1,25 @@
+// #include <stdio.h>
+
+// int main(void) {
+//     int num,num2 = 0;
+//     scanf("%d %d",&num, &num2);
+//     if (num % 2 == 0) {
+//         printf("짝수+");
+//         if (num2 % 2 == 0) {
+//             printf("짝수=짝수");
+//         }
+//         else {
+//             printf("홀수=홀수");
+//         }
+//     }
+//     else {
+//         printf("홀수+");
+//         if (num2 % 2 == 0) {
+//             printf("짝수=홀수");
+//         }
+//         else {
+//             printf("홀수=짝수");
+//         }
+//     }
+//     return 0;
+// }
