d822865c261f42e6c2882629fcd52436122da59d
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-12-22 12:20:16 +0000

    2024/12/22 - 21 : 20

---
+++ .ccls-cache/@home@runner@codeup/codeup_problem@1162.c
@@ -0,0 +1,13 @@
+// #include <stdio.h>
+// int main(void) {
+//     int y,m,d = 0;
+//     scanf("%d %d %d",&y ,&m ,&d);
+
+//     if ((y - m + d) % 10 == 0) {
+//         printf("대박");
+//     }
+//     else {
+//         printf("그럭저럭");
+//     }
+//     return 0;
+// }
