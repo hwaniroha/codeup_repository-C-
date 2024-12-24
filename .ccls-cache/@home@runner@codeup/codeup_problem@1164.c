ab20acd9aae261c5c40bece783007fdcc816842f
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-12-24 05:58:36 +0000

    2024/12/24 - 14 : 58

---
+++ .ccls-cache/@home@runner@codeup/codeup_problem@1164.c
@@ -0,0 +1,14 @@
+// #include <stdio.h>
+// int main(void) {
+//     int n1,n2,n3 = 0;
+//     scanf("%d %d %d", &n1, &n2, &n3);
+//     if (n1 > 170 && n2 > 170 && n3 > 170) {
+//         printf("PASS");
+//     }
+//     else {
+//         printf("CRASH");
+//     }
+
+
+//     return 0;
+// }
