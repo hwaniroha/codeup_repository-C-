53ec8a71d94e939dcd308b9f9ef89b358bd8b1c2
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-12-25 11:43:48 +0000

    2024/12/25 - 20 : 43

---
+++ .ccls-cache/@home@runner@codeup/codeup_problem@1165.c
@@ -0,0 +1,8 @@
+// #include <stdio.h>
+// int main(void) {
+//     int time,score = 0;
+//     scanf("%d %d",&time ,&score);
+
+//     printf("%d",((89 - time) / 5) + score + 1);
+//     return 0;
+// }
