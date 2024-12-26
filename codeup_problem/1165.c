ad6a581336b45437049b14d42e0cb5b3fb54eb9d
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-12-26 03:20:58 +0000

    2024/12/26 - 12 : 20

--- codeup_problem/1165.c
+++ codeup_problem/1165.c
@@ -1,8 +1,20 @@
 // #include <stdio.h>
 // int main(void) {
-//     int time,score = 0;
-//     scanf("%d %d",&time ,&score);
-
-//     printf("%d",((89 - time) / 5) + score + 1);
+//     int num = 0;
+//     scanf("%d", &num);
+//     if (num % 400 == 0) {
+//         printf("Leap");
+//     }
+//     else if (num % 4 == 0) {
+//         if (num % 100 != 0) {
+//             printf("Leap");
+//         }
+//         else {
+//             printf("Normal");
+//         }
+//     }
+//     else {
+//         printf("Normal");
+//     }
 //     return 0;
 // }
