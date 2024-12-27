9e5a78398610a7b97f30e6632f5f75c182891797
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-12-27 01:24:21 +0000

    2024/12/27 - 10 : 24

---
+++ codeup_problem/1170.c
@@ -0,0 +1,12 @@
+// #include <stdio.h>
+// int main(void) {
+//     int a,b,c = 0;
+//     scanf("%d %d %d",&a ,&b, &c);
+//     if (c < 10) {
+//         printf("%d%d%d%d",a ,b ,0 ,c);
+//     }
+//     else {
+//         printf("%d%d%d",a ,b ,c);
+//     }
+//     return 0;
+// }
