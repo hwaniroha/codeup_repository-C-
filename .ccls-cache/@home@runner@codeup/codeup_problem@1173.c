0c8d5658a458402e47028f66d6a4e0cd36d9a2f8
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2024-12-28 14:56:36 +0000

    2024/12/28 - 23 : 56

---
+++ .ccls-cache/@home@runner@codeup/codeup_problem@1173.c
@@ -0,0 +1,19 @@
+// #include <stdio.h>
+// int main(void) {
+//     int hour,minute = 0;
+//     scanf("%d %d",&hour,&minute);
+//     if ((minute - 30) < 0) {
+//         if (hour == 0) {
+//             hour = 24;
+//         }
+//         hour -= 1;
+//         minute = (minute + 60);
+//         printf("%d %d",hour,(minute-30));
+//     }
+//     else {
+//         printf("%d %d",hour,(minute-30));
+//     }
+
+
+//     return 0;
+// }
