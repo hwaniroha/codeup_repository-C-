8350c9947ebb07e43b8ffca2c7bd520d01374cd6
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2025-01-10 11:00:05 +0000

    2024/1/10 - 20 : 00

---
+++ .ccls-cache/@home@runner@workspace/codeup_problem@1620.c
@@ -0,0 +1,27 @@
+// #include <stdio.h>
+// int f(int n) {
+//     int n2 = n;
+//     int result = 0;
+//     while (1) {
+//         if (n2 == 0) {
+//             break;
+//     }
+//         result += (n2 % 10);
+//         n2 /= 10;
+//     }
+//     return result;
+// }
+
+// int main(void) {
+//     int i,sum;
+//     scanf("%d", &i);
+//     while (1) {
+//         if (i < 10) {
+//             break;
+//         }
+//         i = f(i);
+//     }
+//     printf("%d",i);
+
+//     return 0;
+// }
