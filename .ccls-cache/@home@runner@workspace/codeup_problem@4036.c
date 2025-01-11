0bad597daf784ab858f8baec166d7f6347671697
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2025-01-11 13:15:49 +0000

    2024/1/11 - 22 : 15

---
+++ .ccls-cache/@home@runner@workspace/codeup_problem@4036.c
@@ -0,0 +1,16 @@
+1
+  // #include <stdio.h>
+
+// int main(void) {
+//   int n, m = 0;
+//   scanf("%d %d", &n, &m);
+
+//   if ((n + m) % 2 == 0 && (n - m) % 2 == 0) {
+//     int i = (n + m) / 2;
+//     int j = (n - m) / 2;
+//     if (i >= 0 && i <= n && j >= 0 && j <= n) {
+//       printf("%d\n%d", i, j);
+//     }
+//   }
+//   return 0;
+// }
