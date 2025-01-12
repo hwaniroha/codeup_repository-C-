79c7f4736443e054cf1789189a1cc27cbb2a96d3
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2025-01-12 11:42:06 +0000

    2024/1/12 - 20 : 42

---
+++ codeup_problem/1580.c
@@ -0,0 +1,13 @@
+// #include <stdio.h>
+
+// float circle(float num) {
+//     float result = 0;
+//     result = num * num * 3.14;
+//     return result;
+// }
+// main()
+// {
+//     int r;
+//     scanf("%d", &r);
+//     printf("%.2f", circle(r));
+// }
