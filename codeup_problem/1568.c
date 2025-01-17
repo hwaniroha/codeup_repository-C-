bc1b78fbee133c68f152213754330f2ec133813e
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2025-01-17 14:14:13 +0000

    2025/1/17 - 23 : 14

---
+++ codeup_problem/1568.c
@@ -0,0 +1,25 @@
+// #include <stdio.h>
+
+// int n, a, b, d[1010];
+// int maxi(int a, int b) {
+//     int index = a;
+//     int ki = d[a];
+//     for (int i = a; i <= b; i++) {
+//         if (d[i] > ki) {
+//             index = i;
+//             ki = d[i];
+//         }
+//     }
+//     return index;
+// }
+// int main()
+// {
+//   scanf("%d", &n);
+
+//   for(int i=1; i<=n; i++)
+//     scanf("%d", &d[i]);
+
+//   scanf("%d%d", &a, &b);
+
+//   printf("%d\n", maxi(a, b));
+// }
