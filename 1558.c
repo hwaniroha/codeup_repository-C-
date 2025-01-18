81c87a212b5653818b5e8e1d49f7c1fce337c866
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2025-01-18 10:36:12 +0000

    2025/1/18 - 19 : 36

---
+++ 1558.c
@@ -0,0 +1,16 @@
+// #include <stdio.h>
+
+// long long int n;
+
+// long long int f(long long int n) {
+//     while(n>9) {
+//         printf("%lld",n%10);
+//         n/=10;
+//     }
+//     return n;
+// }
+// int main()
+// {
+//   scanf("%lld", &n);
+//   printf("%lld\n", f(n));
+// }
