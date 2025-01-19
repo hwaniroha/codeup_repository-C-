c0687d5aa499f21e9d1694f5a5ec42a18168e549
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2025-01-19 13:31:04 +0000

    2025/1/19 - 22 : 31

---
+++ codeup_problem/1566.c
@@ -0,0 +1,23 @@
+// #include <stdio.h>
+
+// int a, n;
+// long long int pow(int a, int n) {
+//     long long int n3 = a;
+//     if (a == 1) {
+//         return 1;
+//     }
+//     else {
+//         if (n == 0) {
+//             return 1;
+//         }
+//         for (int i = 1; i < n; i++) {
+//             n3 = n3 * a;
+//         }
+//         return n3;
+//     }
+// }
+// int main()
+// {
+//   scanf("%d%d", &a, &n);
+//   printf("%lld\n", pow(a, n));
+// }
