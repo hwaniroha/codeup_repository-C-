cdbb4a5c7bff1b814646235d7e802448139d508c
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2025-01-23 13:35:09 +0000

    2025/1/23 - 22 : 35

---
+++ codeup_problem/1565.c
@@ -0,0 +1,19 @@
+// #include <stdio.h>
+
+// int gcd(int p, int q){ if(p==0) return q; return gcd(q%p, p);}
+
+// long long int lcm(int n1,int n2) {
+//     long long int i = n1;
+//     long long int j = n2;
+//     if (i > j) {
+//             i = n1;
+//             j = n2;
+//         }
+//         return i / gcd(j, i) * j;
+//     }
+// int main()
+// {
+//   int a, b;
+//   scanf("%d%d", &a, &b);
+//   printf("%lld\n", lcm(a, b));
+// }
