dd4e099cd0536bba6649f434b930a115e2125e0f
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2025-01-24 11:55:42 +0000

    2025/1/24 - 20 : 55

---
+++ codeup_problem/1854.c
@@ -0,0 +1,16 @@
+// #include <stdio.h>
+// long long int n,sum;
+// int f(long long int n) {
+//     if(n < 10) {
+//         sum += n;
+//         return 0;
+//     }
+//     float fn = n % 10;
+//     sum += fn;
+//     return(f(n / 10));
+// }
+// int main(void) {
+//     scanf("%lld", &n);
+//     f(n);
+//     printf("%lld", sum);
+// }
