ca6b4078b5ebb2fa0de859bc5d835fd1f93e4b5e
hwaniroha <167195848+hwaniroha@users.noreply.github.com>
2025-01-27 14:08:39 +0000

    2025/1/27 - 23 : 08 Last

---
+++ codeup_problem/3301.c
@@ -0,0 +1,25 @@
+// int main(){
+
+//    int change;
+//    int result=0;
+//    scanf("%d", &change);
+//    result+=change/50000;
+//    change%=50000;
+//    result+=change/10000;
+//    change%=10000;
+//    result+=change/5000;
+//    change%=5000;
+//    result+=change/1000;
+//    change%=1000;
+//    result+=change/500;
+//    change%=500;
+//    result+=change/100;
+//    change%=100;
+//    result+=change/50;
+//    change%=50;
+//    result+=change/10;
+//    change%=10;
+//    printf("%d",result);
+
+
+// }
